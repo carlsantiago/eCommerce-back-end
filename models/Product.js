daf6266150e206873ff91b2f93606535f16850bd
Carl Santiago <4518gg@gmail.com>
2021-12-01 16:53:06 +1100

    Working category routes

--- models/Product.js
+++ models/Product.js
@@ -35,7 +35,7 @@ Product.init(
       }
     },
     category_id: {
-      type: DataTypes.INGEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id'
